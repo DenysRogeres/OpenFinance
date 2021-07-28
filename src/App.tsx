7b54fbd70373cb32d68bc217520b3b31efdcf7f2
DenysRogeres <denysmex@gmail.com>
2021-07-27 22:54:22 -0300

    Adding styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 100px;
+
+`
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>
+        <Title>Hello World</Title>
+      </h1>
     </div>
   );
 }
