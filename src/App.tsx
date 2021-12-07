1f88ba5c0d4d1736ab68bc5651d520dc55c0d3bf
DenysRogeres <dennysrogeres@gmail.com>
2021-12-06 22:27:55 -0300

    Starting project again

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import { GlobalStyle } from './styles/global';
-import { Header } from './components/Header'
-import { Dashboard } from './components/Dashboard';
-import Modal from 'react-modal';
-import { useState } from 'react';
-
 export function App() {
-  const [isNewTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
-    
-    function handleOpenTransactionModal() {
-        setNewTransactionModalOpen(true);
-    }
-
-    function handleCloseTransactionModal() {
-        setNewTransactionModalOpen(false);
-    }
-
   return (
     <div className="App">
-      <GlobalStyle />
-      <Header onNewTransactionModal = {handleOpenTransactionModal}/>
-
-      <Modal 
-        
-        isOpen = {isNewTransactionModalOpen}
-        onRequestClose = {handleCloseTransactionModal}
-        >
-        <h2>Cadastrar transaão</h2>
-      </Modal>
-
-      <Dashboard />
+      <h1>Hello World</h1>
     </div>
   );
 }
-
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer } from 'miragejs';
 
-createServer({
-  routes() {
-    this.namespace = 'api/';
-
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
-  }
-})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -28,4 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
