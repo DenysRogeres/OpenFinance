1ffb07e435f0ed00c3da5b7e4cd5cd98a45e4ca5
DenysRogeres <dennysrogeres@gmail.com>
2021-12-09 23:21:38 -0300

    Adding react modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { GlobalStyled } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import Modal from 'react-modal';
 
 export function App() {
   return (
     <>
       <GlobalStyled/>
       <Header/>
+      <Modal 
+          isOpen={isNewTransacationModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+      >
+          <h2>Cdastrar transação</h2>
+      </Modal>
       <Dashboard/>
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
+import Modal from 'react-modal';
 
 export function Header() {
+    const [isNewTransacationModalOpen, setNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setNewTransactionModalOpen(false);
+    }
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="Logo dt money" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
