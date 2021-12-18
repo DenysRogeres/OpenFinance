9a55ecf248460660d9f7c456eaff052f450b0133
DenysRogeres <dennysrogeres@gmail.com>
2021-12-18 03:12:19 -0300

    Add dinamic data

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,11 +1,22 @@
 import { Container } from "./styles";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionTable() {
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
     useEffect(() => {
         api('/transactions')
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions))
     }, []);
 
     return(
@@ -20,18 +31,14 @@ export function TransactionTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento</td>
-                        <td className="deposit">R$ 1000</td>
-                        <td>Programação</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento</td>
-                        <td className="withdraw">R$ -1000</td>
-                        <td>Programação</td>
-                        <td>20/02/2021</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                        <tr>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>{transaction.amount}</td>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.createdAt}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
