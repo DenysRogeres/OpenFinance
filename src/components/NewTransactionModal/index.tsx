ca5b1e08e0d7ac081124d840eea0ab33db106360
DenysRogeres <dennysrogeres@gmail.com>
2021-12-22 23:40:47 -0300

    Finishing project

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,6 @@
 import Modal from 'react-modal';
 import { FormEvent, useState, useContext } from 'react';
-import { api } from '../../services/api';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
@@ -15,7 +14,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
-    const { createTransaction } = useContext(TransactionsContext);
+    const { createTransaction } = useTransactions();
 
     const [type, setType] = useState('deposit');
     const [title, setTitle] = useState('');
@@ -36,7 +35,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         setTitle('');
         setAmount(0);
         setCategory('');
-        
+
         onRequestClose();
     }
 
